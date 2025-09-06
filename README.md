4fe65257880aa1b4fe0806e72533ef1d8565090e
Zafar205 <mz09206@st.habib.edu.pk>
2025-09-06 21:55:40 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Competitive Programming Essentials, Master Algorithms.
+
+Watched: 12/496
+
+Day 1 :
+
+Macros, code snippets in vscode, header files
---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define print(a) for(auto x : a) cout << x << " "
+
+void solve(){    
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t = 1;
+    while(t--) solve();
+    return 0;
+}
